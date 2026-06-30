b06013c5db68ab730feecdc3c78bede1f3bcb53b
Rajesh Yadav <rajeshyadav2593@yahoo.in>
2026-06-30 19:49:15 +0530

    Initialized Streamlit application

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="SteamBOQ_AI",
+    layout="wide"
+)
+
+st.title("SteamBOQ_AI")
+
+st.write("Welcome to SteamBOQ_AI")
+
+st.subheader("Version 1 - MVP")
